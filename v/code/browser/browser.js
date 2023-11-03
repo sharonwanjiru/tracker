55c3c31d29542366c5033b7d489faa21c79618eb
sharonwanjiru <sharondichu@gmail.com>
2023-11-03 15:32:59 +0300

    changes to  the browser files

--- v/code/browser/browser.js
+++ v/code/browser/browser.js
@@ -1,54 +1,147 @@
-//Import the page and view class from outtlook
+import { exec } from "../../../../schema/v/code/server.js";
+//
 import { page } from "./../../../../outlook/v/code/view.js";
 //
-// The displaying class 
+// The displaying class
 export class browser extends page {
-    root_folder;
     //
     //
-    constructor(root_folder) {
+    constructor() {
         super();
-        this.root_folder = root_folder;
     }
-    // 
-    // This method is used to show the folders inside the digital ocean. 
-    // Below is a dummy of the expected output 
+    //
+    // This method is used to show the folders inside the digital ocean.
+    // Below is a dummy of the expected output
     /*
-  
-    <div id= "root">
-<!--        Mutall Projects Folder -->
-        <details>
-            <summary>Mutall Projects</summary>
-<!--            Mashamba Subfolder-->
-            <details style="margin-left:10px" >
-                <summary >Mashamba</summary>
-                <details style="margin-left:20px">
-                    <summary>V</summary>
-                    <details style="margin-left:30px">
-                        <summary>code</summary>
-                        <p>mashamba.ts</p>
-                        <p>mashamba.html</p>
-                    </details>
-                </details>
-                <details style="margin-left:20px">
-                    <summary>Data</summary>
-                    <p>mashamba.sql</p>
-                </details>
+
+        <div id= "root">
+            <details>
+                <summary>x1</summary>
             </details>
-    </div>
-     */
-    // 
-    //  A method for displaying the server contents as listed in the html above
+            <details>
+                <summary>x2</summary>
+            </details>
+
+            ...
+        </div>
+    */
+    //
+    //  Display the server contents as listed in the html above
     async show_panels() {
-        alert(`You are about to browse folder '${this.root_folder}'`);
         //
-        //Assume the div#root already exists.
-        // Create the details for the root node and place it under the root
+        //Get the root node.
+        const root = this.get_element("root");
         //
-        //Get the child foldersof the root rom teh server
+        //Get the child paths, i.e., files and folders, from the server
+        const paths = await this.get_child_paths("/schema/v/code");
         //
-        //Add the children to to the root node     
+        //Add the children to the root node
+        paths.forEach(path => this.show_child(root, path));
+    }
+    //
+    //Get the child folders of the root from the server and display them
+    async get_child_paths(path) {
         //
+        //Scan the folders on the index page
+        const result = await exec("path", [path, false], //Asuume that path is not a file
+        "scandir", []);
         //
+        // Return the paths
+        return result;
+    }
+    //
+    //Show the given child of the given parent element
+    show_child(parent, child) {
+        //
+        //Define a path object
+        let path;
+        //
+        // Create the path object
+        if (child.is_file)
+            path = new file(parent, child);
+        else
+            path = new folder(parent, child);
+        //
+        //Show the path
+        path.show();
+    }
+}
+//
+// A class for defining a path object
+class path {
+    //
+    constructor() {
+    }
+}
+//
+// Class file displays the file contents
+class file extends page {
+    //
+    constructor(parent, child) {
+        super();
+        //
+        // Initialize parent 
+        this.parent = parent;
+        //
+        // Initialize child
+        this.child = child;
+    }
+    //
+    // Define properties for the file class
+    parent;
+    child;
+    show() {
+        // Create a div to attach the file
+        const file = this.create_element('div', this.parent);
+        //
+        // Attach the file name to the div
+        file.textContent = this.child.name;
+    }
+}
+//
+// Class folder displays the folder contents
+class folder extends page {
+    //
+    constructor(parent, child) {
+        super();
+        //
+        // Initialize parent 
+        this.parent = parent;
+        //
+        // Initialize child
+        this.child = child;
+    }
+    //
+    // Define properties for the file class
+    parent;
+    child;
+    show() {
+        //
+        // Create the details element
+        const details = this.create_element('details', this.parent);
+        //
+        // Add event listener for the details icon
+        details.addEventListener('click', () => {
+            this.details_clicked(this.child);
+        });
+        //
+        // Create the summary elements 
+        const summary = this.create_element('summary', details);
+        //
+        // Attach the folder name to the summary
+        summary.textContent = this.child.name;
+        //
+        // Add event listener for the summary text
+        summary.addEventListener('click', () => { this.summary_clicked(this.child); });
+    }
+    //
+    // The event listener for the details icon
+    details_clicked(fold) {
+        alert(`clicked on the detail icon of ${fold.name}`);
+    }
+    //
+    // The event listener for the summary text
+    summary_clicked(fold) {
+        alert(`clicked on the summary text of ${fold.name}`);
     }
 }
--- v/code/browser/browser.ts
+++ v/code/browser/browser.ts
@@ -1,116 +1,169 @@
+import {exec} from "../../../../schema/v/code/server.js";
 //
-// Import the library
-import * as lib from "../../../../schema/v/code/library";
+import { mutall_error, basic_value } from "./../../../../schema/v/code/schema.js";
 //
-// Import the server
-import * as server from "../../../../schema/v/code/server.js";
+import { page } from "./../../../../outlook/v/code/view.js";
 //
-// Import mutall error from the library
-import {mutall_error} from "./../../../../schema/v/code/schema.js";
-
-//Import the page and view class from outtlook
-import {page} from "./../../../../outlook/v/code/view.js";
-//
-//Define the folders type
-type folder = {
-    path: string;
+// Define the type
+type Ipath = {
+    //
+    //The full name of thepath    
+    path: string; 
+    //
+    //Base nane
     name: string;
+    //
+    //Indicator whether this path is for a file or folder
     is_file: boolean;
-    properties: { [index: string]: lib.basic_value };
-}
+    properties: { [index: string]: basic_value }; //e.g., {date_of_modificatin:'2013-13-04 20:00'}
+};
 //
-// The displaying class 
+// The displaying class
 export class browser extends page {
     //
     //
-    constructor(public root_folder:string) {
-        super();
+    constructor() {
+      super();
     }
-    // 
-    // This method is used to show the folders inside the digital ocean. 
-    // Below is a dummy of the expected output 
+    //
+    // This method is used to show the folders inside the digital ocean.
+    // Below is a dummy of the expected output
     /*
-  
-    <div id= "root">   
-<!--        Mutall Projects Folder -->
-        <details>
-            <summary>Mutall Projects</summary>
-<!--            Mashamba Subfolder-->
-            <details style="margin-left:10px" >
-                <summary >Mashamba</summary>
-                <details style="margin-left:20px">
-                    <summary>V</summary>
-                    <details style="margin-left:30px">
-                        <summary>code</summary>
-                        <p>mashamba.ts</p>
-                        <p>mashamba.html</p>
-                    </details>
-                </details>
-                <details style="margin-left:20px">
-                    <summary>Data</summary>
-                    <p>mashamba.sql</p>
-                </details>
+
+        <div id= "root">   
+            <details>
+                <summary>x1</summary>
             </details>
-    </div>        
-     */
-    // 
-    //  A method for displaying the server contents as listed in the html above
-    async show_panels():Promise<void>{
-        
+            <details>
+                <summary>x2</summary>
+            </details>
+
+            ...
+        </div>        
+    */
+    //
+    //  Display the server contents as listed in the html above
+    async show_panels(): Promise<void> {
         //
-        //Assume the div#root already exists.
-        const div = this.get_element("root");
-        // Create the details for the root node and place it under the root
-        const details:HTMLDetailsElement = this.create_element('details',div);
+        //Get the root node.
+        const root = this.get_element("root");
         //
-        //Get the child folders of the root from the server and display them
-        const child_folder = await this.child_folders();
+        //Get the child paths, i.e., files and folders, from the server
+        const paths: Array<Ipath> = await this.get_child_paths("/schema/v/code");
         //
-        //Add the children to to the root node 
-        this.children(details,child_folder);
-        //
-        //     
-    } 
+        //Add the children to the root node
+        paths.forEach(path=>this.show_child(root, path));
+    }
     //
     //Get the child folders of the root from the server and display them
-    async child_folders():Promise<folder[]>{
+    async get_child_paths(path:string): Promise<Array<Ipath>> {
         //
         //Scan the folders on the index page
-        const folders:Array<folder> = await server.exec('path', ['/', true], 'scandir', []);
+        const result: Array<Ipath> = await exec(
+          "path",
+          [path, false],//Asuume that path is not a file
+          "scandir",
+          []
+        );
         //
-        // Return the folders
-        return folders;
+        // Return the paths
+        return result;
     }
     //
-    //Add the children to to the root node 
-    async children(details:HTMLDetailsElement,folders:folder[]){
+    //Show the given child of the given parent element
+    show_child(parent: HTMLElement, child: Ipath): void{
         //
-        // Check if the returned result is a file or a folder
-        folders.forEach((fold) => {
-            // If it is a file display the file name
-            if (fold.is_file){
-                //
-                // Create a file anchor
-                const file_anchor = this.create_element('p', details);
-                // Give it the namr
-                file_anchor.textContent = fold.name;
-            } else
-            {
-                //
-                // For the folders on the index page
-                const child_details = this.create_element('details', details);
-                //
-                // Attach the folders name  to the summary
-                details.innerHTML = `<summary>${fold.name}</summary>`;
-                // 
-                // If it has children lop through them
-            if (fold) {
-                this.child_folders(details);
-            }
-            }
-        }
-        );
+        //Define a path object
+        let path:path|undefined;
+        //
+        // Create the path object
+        if (child.is_file) path = new file(parent, child)
+        else path = new folder(parent, child);
+        //
+        //Show the path
+        path.show();
     }
-     
-    
+} 
+//
+// A class for defining a path object
+ abstract class path {
+    //
+    constructor(){
+    }
+    //
+    // The showmethod that will be overridden
+    abstract show():void;   
+}
+//
+// Class file displays the file contents
+class file extends page{
+    //
+    constructor(parent:HTMLElement,child:Ipath){
+        super();
+        //
+        // Initialize parent 
+        this.parent = parent;
+        //
+        // Initialize child
+        this.child = child;
+        
+    }
+    //
+    // Define properties for the file class
+    parent: HTMLElement;
+    child: Ipath;
+    show(){
+        // Create a div to attach the file
+        const file: HTMLDivElement = this.create_element('div', this.parent);
+        //
+        // Attach the file name to the div
+        file.textContent = this.child.name;
+    }
+}
+//
+// Class folder displays the folder contents
+class folder extends page implements path{
+    //
+    constructor(parent:HTMLElement,child:Ipath){
+        super();
+        //
+        // Initialize parent 
+        this.parent = parent;
+        //
+        // Initialize child
+        this.child = child;
+    }
+    //
+    // Define properties for the file class
+    parent: HTMLElement;
+    child: Ipath;
+    show(){
+        //
+        // Create the details element
+        const details: HTMLDetailsElement = this.create_element('details', this.parent);
+        //
+        // Add event listener for the details icon
+        details.addEventListener('click', () => {
+            this.details_clicked(this.child);
+        });
+        //
+        // Create the summary elements 
+        const summary: HTMLElement = this.create_element('summary',details);
+        //
+        // Attach the folder name to the summary
+        summary.textContent = this.child.name;
+        //
+        // Add event listener for the summary text
+        summary.addEventListener('click', () => {this.summary_clicked(this.child);});
+    }
+    //
+    // The event listener for the details icon
+    details_clicked(fold:Ipath){
+        alert(`clicked on the detail icon of ${fold.name}`);
+    }
+    //
+    // The event listener for the summary text
+    summary_clicked(fold:Ipath){
+        alert(`clicked on the summary text of ${fold.name}`);
+    }
 }
