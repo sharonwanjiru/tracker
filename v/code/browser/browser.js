a2f833aa964043b5cf09d8a2f8e3bc6307657144
sharonwanjiru <sharondichu@gmail.com>
2023-11-08 12:27:41 +0300

    improved the code structure

--- v/code/browser/browser.js
+++ v/code/browser/browser.js
@@ -1,50 +1,59 @@
 import { exec } from "../../../../schema/v/code/server.js";
 //
-import { page } from "./../../../../outlook/v/code/view.js";
+import { view, page } from "./../../../../outlook/v/code/view.js";
 //
 // The displaying class
 export class browser extends page {
+    root;
     //
-    //
-    constructor() {
+    //Root is the path we will start to scan for files and folders on teh server
+    constructor(root) {
+        //
+        //Initialize the inherited page     
         super();
+        this.root = root;
     }
     //
-    // This method is used to show the folders inside the digital ocean.
+    // This method is used to show the paths,i.e., filesand folders in the current
+    // server, startin from theroot folernamed inthe constructor.
     // Below is a dummy of the expected output
     /*
-
-        <div id= "root">
-            <details>
-                <summary>x1</summary>
-            </details>
-            <details>
-                <summary>x2</summary>
-            </details>
-
-            ...
-        </div>
-    */
+  
+          <div id= "root">
+              <details>
+                  <summary>x1</summary>
+              </details>
+              <details>
+                  <summary>x2</summary>
+              </details>
+  
+              ...
+          </div>
+      */
     //
     //  Display the server contents as listed in the html above
     async show_panels() {
         //
-        //Get the root node.
+        //Get the root node from the current form.
         const root = this.get_element("root");
         //
-        //Get the child paths, i.e., files and folders, from the server
-        const paths = await this.get_child_paths("/schema/v/code");
+        //Get the child paths, i.e., files and folders, of the root foder.
+        //N.B. The fisrt time round, the path isspecified without the document
+        //root component, so, it neess to be adde
+        const paths = await this.get_child_paths(this.root, true);
         //
         //Add the children to the root node
-        paths.forEach(path => this.show_child(root, path));
+        paths.forEach((path) => this.show_child(root, path));
     }
     //
-    //Get the child folders of the root from the server and display them
-    async get_child_paths(path) {
+    //Get the child folders of the given path and display them
+    async get_child_paths(path, add_root) {
         //
-        //Scan the folders on the index page
-        const result = await exec("path", [path, false], //Asuume that path is not a file
-        "scandir", []);
+        //Scan the folders (on the index page??)
+        const result = await exec("path", 
+        //
+        //Assume that path is not a file
+        [path, false, add_root], "scandir", []);
         //
         // Return the paths
         return result;
@@ -67,81 +76,92 @@ export class browser extends page {
     }
 }
 //
-// A class for defining a path object
-class path {
-    //
-    constructor() {
+//This class models paths associated with files and folders in computer disk
+class path extends view {
+    parent;
+    full_name;
+    name;
+    constructor(parent, child) {
+        super();
+        this.parent = parent;
+        this.full_name = child.path;
+        this.name = child.name;
     }
 }
 //
 // Class file displays the file contents
-class file extends page {
+class file extends path {
     //
+    div;
     constructor(parent, child) {
-        super();
         //
-        // Initialize parent 
-        this.parent = parent;
-        //
-        // Initialize child
-        this.child = child;
+        super(parent, child);
+        this.div = this.create_element("div", this.parent);
     }
-    //
-    // Define properties for the file class
-    parent;
-    child;
+    get proxy() { return this.div; }
+    //Display a file, thus implementeing the abstarct version
     show() {
-        // Create a div to attach the file
-        const file = this.create_element('div', this.parent);
         //
         // Attach the file name to the div
-        file.textContent = this.child.name;
+        this.div.textContent = this.name;
     }
 }
 //
 // Class folder displays the folder contents
-class folder extends page {
+class folder extends path {
+    //
+    // The summary in which to attach a text???????
+    summary;
+    details;
     //
     constructor(parent, child) {
-        super();
         //
-        // Initialize parent 
-        this.parent = parent;
-        //
-        // Initialize child
-        this.child = child;
-    }
-    //
-    // Define properties for the file class
-    parent;
-    child;
-    show() {
+        //Initialize te parent system
+        super(parent, child);
         //
         // Create the details element
-        const details = this.create_element('details', this.parent);
+        this.details = this.create_element("details", this.parent);
         //
         // Add event listener for the details icon
-        details.addEventListener('click', () => {
-            this.details_clicked(this.child);
-        });
+        this.details.onclick = () => this.details_clicked(this.name);
         //
-        // Create the summary elements 
-        const summary = this.create_element('summary', details);
-        //
-        // Attach the folder name to the summary
-        summary.textContent = this.child.name;
+        // Create the summary elements
+        this.summary = this.create_element("summary", this.details);
         //
         // Add event listener for the summary text
-        summary.addEventListener('click', () => { this.summary_clicked(this.child); });
+        this.summary.click = () => this.summary_clicked(this.name);
+    }
+    get proxy() { return this.details; }
+    //
+    // Define properties for the file class
+    async show() {
+        //
+        // Attach the folder name to the summary
+        this.summary.textContent = this.name;
+        //
+        // Check if the folder has children 
+        //const folder = await this.get_folders(this.full_name,false);
+        //
+        //Check if thereare children inside the folders
+    }
+    //
+    // Get the folders inside the parent folder
+    async get_folders(full_name, add_root) {
+        //
+        // Get the folders
+        const folders = await exec("path", [full_name, add_root], "scandir", []);
+        //
+        // Return the folders
+        return folders;
     }
     //
     // The event listener for the details icon
-    details_clicked(fold) {
-        alert(`clicked on the detail icon of ${fold.name}`);
+    details_clicked(name) {
+        alert(`clicked on the detail icon of ${this.name}`);
     }
     //
     // The event listener for the summary text
-    summary_clicked(fold) {
-        alert(`clicked on the summary text of ${fold.name}`);
+    summary_clicked(name) {
+        alert(`clicked on the summary text of ${this.name}`);
     }
 }
--- v/code/browser/browser.ts
+++ v/code/browser/browser.ts
@@ -1,34 +1,40 @@
-import {exec} from "../../../../schema/v/code/server.js";
+import { exec } from "../../../../schema/v/code/server.js";
 //
-import { mutall_error, basic_value } from "./../../../../schema/v/code/schema.js";
+import {
+  mutall_error,
+  basic_value,
+} from "./../../../../schema/v/code/schema.js";
 //
-import { page } from "./../../../../outlook/v/code/view.js";
+import { view, page } from "./../../../../outlook/v/code/view.js";
 //
 // Define the type
 type Ipath = {
-    //
-    //The full name of thepath    
-    path: string; 
-    //
-    //Base nane
-    name: string;
-    //
-    //Indicator whether this path is for a file or folder
-    is_file: boolean;
-    properties: { [index: string]: basic_value }; //e.g., {date_of_modificatin:'2013-13-04 20:00'}
+  //
+  //The full name of thepath
+  path: string;
+  //
+  //Base nane
+  name: string;
+  //
+  //Indicator whether this path is for a file or folder
+  is_file: boolean;
+  properties: { [index: string]: basic_value }; //e.g., {date_of_modificatin:'2013-13-04 20:00'}
 };
 //
 // The displaying class
 export class browser extends page {
-    //
-    //
-    constructor() {
-      super();
-    }
-    //
-    // This method is used to show the folders inside the digital ocean.
-    // Below is a dummy of the expected output
-    /*
+  //
+  //Root is the path we will start to scan for files and folders on teh server
+  constructor(public root:string) {
+   //
+    //Initialize the inherited page     
+    super();
+  }
+  //
+  // This method is used to show the paths,i.e., filesand folders in the current
+  // server, startin from theroot folernamed inthe constructor.
+  // Below is a dummy of the expected output
+  /*
 
         <div id= "root">   
             <details>
@@ -41,129 +47,165 @@ export class browser extends page {
             ...
         </div>        
     */
+  //
+  //  Display the server contents as listed in the html above
+  async show_panels(): Promise<void> {
     //
-    //  Display the server contents as listed in the html above
-    async show_panels(): Promise<void> {
-        //
-        //Get the root node.
-        const root = this.get_element("root");
-        //
-        //Get the child paths, i.e., files and folders, from the server
-        const paths: Array<Ipath> = await this.get_child_paths("/schema/v/code");
-        //
-        //Add the children to the root node
-        paths.forEach(path=>this.show_child(root, path));
-    }
+    //Get the root node from the current form.
+    const root = this.get_element("root");
     //
-    //Get the child folders of the root from the server and display them
-    async get_child_paths(path:string): Promise<Array<Ipath>> {
-        //
-        //Scan the folders on the index page
-        const result: Array<Ipath> = await exec(
-          "path",
-          [path, false],//Asuume that path is not a file
-          "scandir",
-          []
-        );
-        //
-        // Return the paths
-        return result;
-    }
+    //Get the child paths, i.e., files and folders, of the root foder.
+    //N.B. The fisrt time round, the path isspecified without the document
+    //root component, so, it neess to be adde
+    const paths: Array<Ipath> = await this.get_child_paths(this.root, true);
     //
-    //Show the given child of the given parent element
-    show_child(parent: HTMLElement, child: Ipath): void{
-        //
-        //Define a path object
-        let path:path|undefined;
-        //
-        // Create the path object
-        if (child.is_file) path = new file(parent, child)
-        else path = new folder(parent, child);
-        //
-        //Show the path
-        path.show();
-    }
-} 
+    //Add the children to the root node
+    paths.forEach((path) => this.show_child(root, path));
+  }
+  //
+  //Get the child folders of the given path and display them
+  async get_child_paths(path: string, add_root:boolean): Promise<Array<Ipath>> {
+    //
+    //Scan the folders (on the index page??)
+    const result: Array<Ipath> = await exec(
+      "path",
+      //
+      //Assume that path is not a file
+      [path, false, add_root], 
+      "scandir",
+      []
+    );
+    //
+    // Return the paths
+    return result;
+  }
+  //
+  //Show the given child of the given parent element
+  show_child(parent: HTMLElement, child: Ipath): void {
+    //
+    //Define a path object
+    let path: path | undefined;
+    //
+    // Create the path object
+    if (child.is_file) path = new file(parent, child);
+    else path = new folder(parent, child);
+    //
+    //Show the path
+    path.show();
+  }
+}
 //
-// A class for defining a path object
- abstract class path {
-    //
-    constructor(){
+//This class models paths associated with files and folders in computer disk
+abstract class path extends view {
+
+    //Proxy is the HTML element that represents path visually.
+    //public proxy:HTMLElement;
+    abstract get proxy():HTMLElement;
+    abstract set proxy(element:HTMLElement);
+
+    public full_name: string;
+    public name: string;
+    
+    constructor(public parent: HTMLElement, child: Ipath) {
+      super();
+      this.full_name = child.path;
+      this.name = child.name;
+
     }
     //
-    // The showmethod that will be overridden
-    abstract show():void;   
+    // The show method must be implemented by extensions of this class
+    abstract show(): void;
 }
 //
 // Class file displays the file contents
-class file extends page{
+class file extends path {
     //
-    constructor(parent:HTMLElement,child:Ipath){
-        super();
-        //
-        // Initialize parent 
-        this.parent = parent;
-        //
-        // Initialize child
-        this.child = child;
-        
+    public div:HTMLElement;
+
+    constructor(parent: HTMLElement, child: Ipath) {
+      //
+      super(parent, child);
+
+      this.div = this.create_element("div", this.parent);
     }
-    //
-    // Define properties for the file class
-    parent: HTMLElement;
-    child: Ipath;
-    show(){
-        // Create a div to attach the file
-        const file: HTMLDivElement = this.create_element('div', this.parent);
+
+    get proxy():HTMLElement{return this.div;}
+
+    //Display a file, thus implementeing the abstarct version
+    show() {
         //
         // Attach the file name to the div
-        file.textContent = this.child.name;
+        this.div.textContent = this.name;
     }
 }
 //
 // Class folder displays the folder contents
-class folder extends page implements path{
+class folder extends path {
+  //
+  // The summary in which to attach a text???????
+  private summary: HTMLElement;
+
+  details:HTMLDetailsElement;
+
+  //
+  constructor(parent: HTMLElement, child: Ipath) {
     //
-    constructor(parent:HTMLElement,child:Ipath){
-        super();
-        //
-        // Initialize parent 
-        this.parent = parent;
-        //
-        // Initialize child
-        this.child = child;
-    }
+    //Initialize te parent system
+    super(parent, child);
     //
-    // Define properties for the file class
-    parent: HTMLElement;
-    child: Ipath;
-    show(){
-        //
-        // Create the details element
-        const details: HTMLDetailsElement = this.create_element('details', this.parent);
-        //
-        // Add event listener for the details icon
-        details.addEventListener('click', () => {
-            this.details_clicked(this.child);
-        });
-        //
-        // Create the summary elements 
-        const summary: HTMLElement = this.create_element('summary',details);
-        //
-        // Attach the folder name to the summary
-        summary.textContent = this.child.name;
-        //
-        // Add event listener for the summary text
-        summary.addEventListener('click', () => {this.summary_clicked(this.child);});
-    }
+    // Create the details element
+    this.details = this.create_element("details", this.parent);
     //
-    // The event listener for the details icon
-    details_clicked(fold:Ipath){
-        alert(`clicked on the detail icon of ${fold.name}`);
-    }
+    // Add event listener for the details icon
+    this.details.onclick = () => this.details_clicked(this.name);
     //
-    // The event listener for the summary text
-    summary_clicked(fold:Ipath){
-        alert(`clicked on the summary text of ${fold.name}`);
-    }
+    // Create the summary elements
+    this.summary = this.create_element("summary", this.details);
+    //
+    // Add event listener for the summary text
+    this.summary.click = () =>this.summary_clicked(this.name);
+    
+  }
+
+
+  get proxy():HTMLDetailsElement{return this.details;}
+
+  //
+  // Define properties for the file class
+  async show() {
+    //
+    // Attach the folder name to the summary
+    this.summary.textContent = this.name;
+    //
+    // Check if the folder has children 
+    //const folder = await this.get_folders(this.full_name,false);
+    //
+    //Check if thereare children inside the folders
+     
+   }
+  //
+  // Get the folders inside the parent folder
+  async get_folders(full_name:String, add_root:boolean):Promise<Array<Ipath>>{
+      //
+      // Get the folders
+      const folders: Array<Ipath> = await exec(
+      "path",
+      [full_name, add_root],
+      "scandir",
+      []
+    );
+    //
+    // Return the folders
+    return folders;
+  }
+  //
+  // The event listener for the details icon
+  details_clicked(name: String) {
+    alert(`clicked on the detail icon of ${this.name}`);
+  }
+  //
+  // The event listener for the summary text
+  summary_clicked(name: String) {
+    alert(`clicked on the summary text of ${this.name}`);
+  }
 }
